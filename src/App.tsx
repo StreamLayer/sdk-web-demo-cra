82400b5877c2113e191f8c0039f20a7ac4f2d9a7
renat <renat0014@gmail.com>
2023-12-01 00:48:56 +0400

    feat: update integration example

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react';
-import { StreamLayerProvider, StreamLayerSDKReact, useStreamLayer } from '@streamlayer/react'
+import { useState } from 'react'
+import { StreamLayerLogin, MastersStreamLayerProvider, MastersStreamLayerSDKReact, StreamLayerSDKPoints } from '@streamlayer/react/masters'
 import '@streamlayer/react/style.css'
 
-const EventInput = () => {
-  const sdk = useStreamLayer()
-  const [event, setEvent] = useState('')
-  const startEventSession = () => {
-    sdk?.createEventSession(event)
-  }
+function App() {
+  const [user, setUser] = useState({ token: '', schema: '' })
 
-  if (!sdk) {
-    return null
+  const submitUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const data = new FormData(e.currentTarget)
+
+    const token = data.get('token') as string
+    const schema = data.get('schema') as string
+
+    setUser({ token, schema })
   }
 
   return (
-    <div>
-      <input type="text" value={event} onChange={e => setEvent(e.target.value)} />
-      <button onClick={startEventSession}>set</button>
+    <div className='app-div'>
+      <form className='auth-form' onSubmit={submitUser}>
+        <div>
+          <label htmlFor="token">token</label>
+          <input type="text" id="token" name="token" />
+        </div>
+        <div>
+          <label htmlFor="schema">schema</label>
+          <input type="text" id="schema" name="schema" />
+        </div>
+        <div>
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <MastersStreamLayerProvider sdkKey={process.env.REACT_APP_SDK_KEY || ''}>
+        <div className='points'>
+          <StreamLayerSDKPoints />
+        </div>
+        <StreamLayerLogin token={user.token} schema={user.schema} />
+        <MastersStreamLayerSDKReact>
+            {({ enableSdk, disableSdk }) => (
+              <div>
+                <button onClick={() => enableSdk('733')}>enable</button>
+                <button onClick={disableSdk}>disable</button>
+              </div>
+            )}
+          </MastersStreamLayerSDKReact>
+      </MastersStreamLayerProvider>
     </div>
   )
 }
 
-function App() {
-  return (
-    <div className='App'>
-      <StreamLayerProvider sdkKey={process.env.REACT_APP_SDK_KEY}>
-        <EventInput />
-        <StreamLayerSDKReact />
-      </StreamLayerProvider>
-    </div>
-  );
-}
-
-export default App;
+export default App
