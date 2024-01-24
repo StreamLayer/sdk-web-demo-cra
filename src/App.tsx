00711645682936618db1ca95ed1a71a9666b0c77
renat <renat0014@gmail.com>
2024-01-24 06:25:50 +0400

    fix: demo app cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { StreamLayerLogin, MastersStreamLayerProvider, MastersStreamLayerSDKReact, StreamLayerSDKPoints } from '@streamlayer/react/masters'
 import '@streamlayer/react/style.css'
 
+const EVENT_ID = process.env.REACT_APP_EVENT_ID || ''
+const SDK_KEY = process.env.REACT_APP_SDK_KEY || ''
+const PRODUCTION = process.env.REACT_APP_PRODUCTION === 'true'
+
 function App() {
   const [user, setUser] = useState({ token: '', schema: '' })
 
@@ -17,33 +21,37 @@ function App() {
 
   return (
     <div className='app-div'>
-      <form className='auth-form' onSubmit={submitUser}>
-        <div>
-          <label htmlFor="token">token</label>
-          <input type="text" id="token" name="token" />
-        </div>
-        <div>
-          <label htmlFor="schema">schema</label>
-          <input type="text" id="schema" name="schema" />
-        </div>
-        <div>
-          <button type="submit">submit</button>
-        </div>
-      </form>
-      <MastersStreamLayerProvider sdkKey={process.env.REACT_APP_SDK_KEY || ''}>
-        <div className='points'>
-          <StreamLayerSDKPoints />
-        </div>
-        <StreamLayerLogin token={user.token} schema={user.schema} />
-        <MastersStreamLayerSDKReact>
-            {({ activateEventWithId, deactivate }) => (
-              <div>
-                <button onClick={() => activateEventWithId('733')}>enable</button>
-                <button onClick={deactivate}>disable</button>
-              </div>
-            )}
-          </MastersStreamLayerSDKReact>
-      </MastersStreamLayerProvider>
+      <div className='app-container'>
+        <MastersStreamLayerProvider sdkKey={SDK_KEY} production={PRODUCTION}>
+          <div className='points'>
+            <StreamLayerSDKPoints />
+          </div>
+          <StreamLayerLogin token={user.token} schema={user.schema} />
+          <MastersStreamLayerSDKReact>
+              {({ activateEventWithId, deactivate }) => (
+                <div>
+                  <button onClick={() => activateEventWithId(EVENT_ID)}>Open event {EVENT_ID}</button>
+                  <button onClick={deactivate}>Close event</button>
+                </div>
+              )}
+            </MastersStreamLayerSDKReact>
+        </MastersStreamLayerProvider>
+      </div>
+      <div className='settings'>
+        <form className='auth-form' onSubmit={submitUser}>
+          <div>
+            <label htmlFor="token">token</label>
+            <input type="text" id="token" name="token" />
+          </div>
+          <div>
+            <label htmlFor="schema">schema</label>
+            <input type="text" id="schema" name="schema" />
+          </div>
+          <div>
+            <button type="submit">submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
