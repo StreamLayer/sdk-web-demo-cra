e2f912a0c65c0b3e754fe7ffbca4ae2e19dfbec2
renat <renat0014@gmail.com>
2024-06-27 15:18:12 +0200

    feat: update deps, test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,9 +6,9 @@ import App from "./App";
 
 describe("App", () => {
   it("renders App component", async () => {
-    render(<App />);
-    const disableButton = screen.getByText('Close event');
+    const { container } = render(<App />);
+    const internalContainer = container.getElementsByClassName('StreamLayerSDKTheme')[0];
 
-    expect(disableButton).toBeDefined();
+    expect(internalContainer).toBeDefined();
   });
 });
